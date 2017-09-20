50d46bbc48ce88f3933e3bc778020811df093434
lakepics <lkepics@kepics.com>
2017-09-20 09:45:04 -0700

    moving to new local dev env

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -19,6 +19,7 @@
     'common': {
       init: function() {
         // JavaScript to be fired on all pages
+          $('.menu-item-has-children').dropdown();
       },
       finalize: function() {
         // JavaScript to be fired on all pages, after page specific JS is fired
