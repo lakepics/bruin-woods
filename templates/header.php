914d5a5938387f44714d59d9844cec94a61ccd9d
lakepics <lkepics@kepics.com>
2017-09-20 10:26:50 -0700

    fix path to logo

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><img class="logo" src="/bw/wp-content/themes/bw/dist/images/logo.svg" alt="UCLA Bruin Woods Family Resort at Lake Arrowhead"></a>
+    <nav class="nav-primary" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
